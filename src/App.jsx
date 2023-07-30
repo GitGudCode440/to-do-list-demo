8fc4ac7fb73161f6a5273c764b40186c70006861
M. Mahad Hashmi <m.mahadhashmi@gmail.com>
2023-07-30 21:05:52 +0500

    Added a TaskForm, tasks to App, and contained it

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import './App.css'
+import TaskForm from './TaskForm'
+import Tasks from './Tasks'
 
-function App() {
+export default function App() {
   return (
-    <h1>Hello There!!!</h1>
+    <div className="flex-center">
+      <div className='
+        border-2 
+        border-gray-800 bg-gray-300 
+        w-80 p-8 rounded-lg'>
+ 
+        <TaskForm></TaskForm>
+        <Tasks></Tasks>
+      </div>
+    </div>
   )
 }
 
-export default App
+
